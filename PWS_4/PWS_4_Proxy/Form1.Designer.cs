a99a7fd927f50de0822863d0f63e20b70e95e493
Андрей Чаевский <andreichayeuski@gmail.com>
2019-09-18 23:54:11 +0300

    lab 4 tasks 1-15

---
+++ PWS_4/PWS_4_Proxy/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PWS_4_Proxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.result_3 = new System.Windows.Forms.TextBox();
+            this.result_2 = new System.Windows.Forms.TextBox();
+            this.result_1 = new System.Windows.Forms.TextBox();
+            this.cav = new System.Windows.Forms.Button();
+            this.d2 = new System.Windows.Forms.TextBox();
+            this.d1 = new System.Windows.Forms.TextBox();
+            this.i2 = new System.Windows.Forms.TextBox();
+            this.i1 = new System.Windows.Forms.TextBox();
+            this.s2 = new System.Windows.Forms.TextBox();
+            this.s1 = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.y = new System.Windows.Forms.TextBox();
+            this.x = new System.Windows.Forms.TextBox();
+            this.getSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // result_3
+            // 
+            this.result_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result_3.Location = new System.Drawing.Point(589, 333);
+            this.result_3.Name = "result_3";
+            this.result_3.Size = new System.Drawing.Size(155, 36);
+            this.result_3.TabIndex = 43;
+            // 
+            // result_2
+            // 
+            this.result_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result_2.Location = new System.Drawing.Point(589, 279);
+            this.result_2.Name = "result_2";
+            this.result_2.Size = new System.Drawing.Size(155, 36);
+            this.result_2.TabIndex = 42;
+            // 
+            // result_1
+            // 
+            this.result_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result_1.Location = new System.Drawing.Point(589, 225);
+            this.result_1.Name = "result_1";
+            this.result_1.Size = new System.Drawing.Size(155, 36);
+            this.result_1.TabIndex = 41;
+            // 
+            // cav
+            // 
+            this.cav.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cav.Location = new System.Drawing.Point(416, 219);
+            this.cav.Name = "cav";
+            this.cav.Size = new System.Drawing.Size(148, 150);
+            this.cav.TabIndex = 40;
+            this.cav.Text = "cav";
+            this.cav.UseVisualStyleBackColor = true;
+            this.cav.Click += new System.EventHandler(this.Cav_Click);
+            // 
+            // d2
+            // 
+            this.d2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d2.Location = new System.Drawing.Point(242, 333);
+            this.d2.Name = "d2";
+            this.d2.Size = new System.Drawing.Size(155, 36);
+            this.d2.TabIndex = 39;
+            // 
+            // d1
+            // 
+            this.d1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.d1.Location = new System.Drawing.Point(56, 333);
+            this.d1.Name = "d1";
+            this.d1.Size = new System.Drawing.Size(155, 36);
+            this.d1.TabIndex = 38;
+            // 
+            // i2
+            // 
+            this.i2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.i2.Location = new System.Drawing.Point(242, 279);
+            this.i2.Name = "i2";
+            this.i2.Size = new System.Drawing.Size(155, 36);
+            this.i2.TabIndex = 37;
+            // 
+            // i1
+            // 
+            this.i1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.i1.Location = new System.Drawing.Point(56, 279);
+            this.i1.Name = "i1";
+            this.i1.Size = new System.Drawing.Size(155, 36);
+            this.i1.TabIndex = 36;
+            // 
+            // s2
+            // 
+            this.s2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.s2.Location = new System.Drawing.Point(242, 225);
+            this.s2.Name = "s2";
+            this.s2.Size = new System.Drawing.Size(155, 36);
+            this.s2.TabIndex = 35;
+            // 
+            // s1
+            // 
+            this.s1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.s1.Location = new System.Drawing.Point(56, 225);
+            this.s1.Name = "s1";
+            this.s1.Size = new System.Drawing.Size(155, 36);
+            this.s1.TabIndex = 34;
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(589, 107);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(155, 36);
+            this.result.TabIndex = 33;
+            // 
+            // y
+            // 
+            this.y.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.y.Location = new System.Drawing.Point(56, 141);
+            this.y.Name = "y";
+            this.y.Size = new System.Drawing.Size(155, 36);
+            this.y.TabIndex = 32;
+            // 
+            // x
+            // 
+            this.x.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.x.Location = new System.Drawing.Point(56, 81);
+            this.x.Name = "x";
+            this.x.Size = new System.Drawing.Size(155, 36);
+            this.x.TabIndex = 31;
+            // 
+            // getSum
+            // 
+            this.getSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.getSum.Location = new System.Drawing.Point(249, 81);
+            this.getSum.Name = "getSum";
+            this.getSum.Size = new System.Drawing.Size(148, 96);
+            this.getSum.TabIndex = 30;
+            this.getSum.Text = "ref";
+            this.getSum.UseVisualStyleBackColor = true;
+            this.getSum.Click += new System.EventHandler(this.GetSum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result_3);
+            this.Controls.Add(this.result_2);
+            this.Controls.Add(this.result_1);
+            this.Controls.Add(this.cav);
+            this.Controls.Add(this.d2);
+            this.Controls.Add(this.d1);
+            this.Controls.Add(this.i2);
+            this.Controls.Add(this.i1);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.y);
+            this.Controls.Add(this.x);
+            this.Controls.Add(this.getSum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox result_3;
+        private System.Windows.Forms.TextBox result_2;
+        private System.Windows.Forms.TextBox result_1;
+        private System.Windows.Forms.Button cav;
+        private System.Windows.Forms.TextBox d2;
+        private System.Windows.Forms.TextBox d1;
+        private System.Windows.Forms.TextBox i2;
+        private System.Windows.Forms.TextBox i1;
+        private System.Windows.Forms.TextBox s2;
+        private System.Windows.Forms.TextBox s1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.TextBox y;
+        private System.Windows.Forms.TextBox x;
+        private System.Windows.Forms.Button getSum;
+    }
+}
+
---
+++ PWS_4/PWS_4_Proxy/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWS_4_Proxy
+{
+    public partial class Form1 : Form
+    {
+        PWS_4_Proxy.Proxy.Simplex proxyClient;
+        public Form1()
+        {
+            InitializeComponent();
+            proxyClient = new PWS_4_Proxy.Proxy.Simplex();
+        }
+
+        private void GetSum_Click(object sender, EventArgs e)
+        {
+            result.ForeColor = Color.Black;
+            int val1, val2;
+            if (int.TryParse(x.Text.ToString(), out val1) && int.TryParse(y.Text.ToString(), out val2))
+            {
+                result.Text = proxyClient.Add(val1, val2).ToString();
+            }
+            else
+            {
+                result.ForeColor = Color.Red;
+                result.Text = "Error!";
+            }
+        }
+
+        private void Cav_Click(object sender, EventArgs e)
+        {
+            var msu1 = new PWS_4_Proxy.Proxy.A();
+            msu1.s = s1.Text;
+            msu1.k = int.Parse(i1.Text);
+            msu1.f = float.Parse(d1.Text);
+
+            var msu2 = new PWS_4_Proxy.Proxy.A();
+            msu2.s = s2.Text;
+            msu2.k = int.Parse(i2.Text);
+            msu2.f = float.Parse(d2.Text);
+
+            var result = proxyClient.Sum(msu1, msu2);
+
+            result_1.Text = result.s;
+            result_2.Text = result.k.ToString();
+            result_3.Text = result.f.ToString();
+        }
+    }
+}
