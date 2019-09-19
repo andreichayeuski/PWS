473818725b0348e8aad22b6500bd90093bb7aa60
Андрей Чаевский <andreichayeuski@gmail.com>
2019-09-20 00:39:20 +0300

    lab 5

---
+++ PWS_5/PWS_5_Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWS_5_Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simplex.Service1Client service1Client = new Simplex.Service1Client();
+
+            var sumResult = service1Client.Sum(new Simplex.A { f = 1.1f, k = 1, s = "1" }, new Simplex.A { f = 5.3f, k = 2, s = "231" });
+            Console.WriteLine($"Sum\nf = {sumResult.f}\nk = {sumResult.k}\ns = {sumResult.s}");
+
+            
+            Console.WriteLine($"\n\nConcat\nresult = " + service1Client.Concat(sumResult.s, sumResult.f));
+
+
+            Console.WriteLine($"\n\nAdd\nresult = " + service1Client.Add(sumResult.k, 8));
+
+            service1Client.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PWS_5/PWS_5_Host_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWS_5_Host_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Simplex.Service1Client service1Client = new Simplex.Service1Client("NetTcpBinding_IService1");
+
+            var sumResult = service1Client.Sum(new PWS_5_WCF.A { f = 1.3f, k = 2, s = "111" }, new PWS_5_WCF.A { f = 5.3f, k = 2, s = "333" });
+            Console.WriteLine($"Sum\nf = {sumResult.f}\nk = {sumResult.k}\ns = {sumResult.s}");
+
+
+            Console.WriteLine($"\n\nConcat\nresult = " + service1Client.Concat(sumResult.s, sumResult.f));
+
+
+            Console.WriteLine($"\n\nAdd\nresult = " + service1Client.Add(sumResult.k, 8));
+
+            service1Client.Close();
+            
+            Console.ReadKey();
+        }
+    }
+}
